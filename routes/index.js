4626bfddcbea18b273be6846fbfa709af0cde754
Eric VanKirk <eric@ericvankirk.com>
2022-09-27 22:17:53 -0500

    index.js routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = requier('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+nodule.exports = router;
