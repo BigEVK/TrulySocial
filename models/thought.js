b26bb72a8c96defb5949dc3bb790ad8929474fe1
Eric VanKirk <eric@ericvankirk.com>
2022-09-27 22:07:48 -0500

    thought schema

--- models/thought.js
+++ models/thought.js
@@ -1,27 +1,75 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
-        type: Required,
+        required: true,
         // Must be bet 1 and 280 characters
+        minlength: 1,
+        maxlength: 280
     },
     createdAt: {
         type: Date,
         // set default value to current timestamp
+        default: Date.now,
         // Use a getter method to format the timestamp on query
+        get: createdAtVal => moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a')
     },
     username: {
         // the user that created this thought
         type: String,
-        type: Required
+        required: true
     },
-    reactions: {
+    reactions: [ReactionSchema]
         // these are like replies
         // array of nested documents created with the 'reactionSchema'      
     },
-});
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
 // Schema Settings
 // Create a virtual called 'reactionCount' that retrieves the length
 //  of the thought's 'reactions' array field on query.
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        rewuired: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a')
+    }
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length:
+});
+
+const Thought = model('Thought, ThoughtSchema');
+
+module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -1,22 +1,49 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 const UserSchema = new Schema({
     userName: {
         type: String,
-        type: Unique,
-        type: Required,
-        type: Trimmed
+        unique: true,
+        required: true,
+        trim: true
     },
     email: {
         type: String,
-        type: Required,
-        type: Unique,
+        required: true,
+        unique: true,
      // type: Must match a valid e-mail address (look into Mongoose's matching validation) 
+        match: [/.+@.+\..+/]
     },
-    thoughts: [],
-    friends: []
-});
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
 
 // Schema Settings
 // Create a virtual called 'friendCount' that retrieves the
 //  length of the user's 'friends' array field on query.
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+const User = model('User', UserSchema);
+
+
+module.exports = User;
